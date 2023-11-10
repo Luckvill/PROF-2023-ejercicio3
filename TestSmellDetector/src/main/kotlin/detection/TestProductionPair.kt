4b212344bd7154d685f447866a1c7262734b4664
Luckvill <118052157+Luckvill@users.noreply.github.com>
2023-11-10 13:07:56 +0100

    Delete TestSmellDetector directory

--- TestSmellDetector/src/main/kotlin/detection/TestProductionPair.kt
+++
@@ -1,8 +0,0 @@
-package detection
-
-data class TestProductionPair(
-        val testClass: String = "",
-        val productionClass: String = "",
-        val testClassPath: String,
-        val productionClassPath: String
-)
